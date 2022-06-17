1744caee090d17ec186e689de5862f2ab28a0d4f
Josh Greiff <josh@greiff.org>
2022-06-16 23:51:59 -0400

    added thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,13 @@
+const { Thought, User } = require('../models')
+
+const thoughtController = {
+    // add thought to a user
+    addThought(){
+        
+    },
+    removeThought(){
+
+    }
+}
+
+module.exports = thoughtController
