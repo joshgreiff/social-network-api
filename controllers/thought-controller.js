5c3ff6db5da526370be96a5a5cb39472bbd7caab
Josh Greiff <josh@greiff.org>
2022-06-17 00:21:46 -0400

    working thought route

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,11 +2,47 @@ const { Thought, User } = require('../models')
 
 const thoughtController = {
     // add thought to a user
-    addThought(){
-        
+    addThought({ params, body }, res){
+        console.log(body)
+        Thought.create(body)
+        .then(({_id}) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            )
+        })
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({ message: 'No user found with this id!' })
+                return
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => res.json(err))
     },
-    removeThought(){
-
+    // remove thought
+    removeThought({ params }, res){
+        Thought.findOneAndDelete({ _id: params.userId })
+            .then(deletedThought => {
+                if(!deletedThought){
+                    res.status(404).json({ message: 'No thought found with this id' })
+                    return
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull : { thought: params.thoughtId } },
+                    { new: true }
+                )
+            })
+            .then(dbUserData => {
+                if(!dbUserData){
+                    res.status(404).json({ message: 'No user found with this id' })
+                    return
+                }
+                res.json(dbUserData)
+            })
+            .catch(err => res.json(err))
     }
 }
 
